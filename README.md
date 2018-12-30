5ff6c9d67c7f01c152b983e6ac9defeb1a5e0e7f
TheCodePack <42550184+TheCodePack@users.noreply.github.com>
2018-12-30 17:35:56 -0500

    Add files via upload
    Backing up broken repo

---
+++ README.md
@@ -0,0 +1,43 @@
+# Bitcoin Monitor
+
+A simple and user friendly app that allows users to monitor the price of bitcoin and see news articles about the currency.
+
+### Working Prototype
+
+TBD
+
+
+## Site Map: MVP 
+
+![Site Map](https://docs.google.com/presentation/d/1Mvb8AqI5XZ-XjbEwC9K9UFwz-L4wg-Hpb6zZq1dkdXU/edit#slide=id.g4be1a5f606_0_0)
+
+
+## UX & User Stories: MVP
+
+**Welcome Page** 
+
+As a user, I want to understand what the app is and learn where the data is sourced so that I can determine the utility/validity of the app.
+
+As a user, I need to be able to click a button to move forward from the landing page and utilize the app.
+
+![Landing Page Design](https://github.com/TheCodePack/bitcoin-price-monitor-api-capstone/blob/master/wireframes/welcome-page.jpg)
+
+
+**Dashboard Page** 
+
+As a user, I need to be able to select from several timeframes so that I can view current and historical data in the main chart.
+
+As a user, I need each button representing timeframes to change the range of historical data displayed in the chart. 
+
+As a user, I need to be able to pick the currency I want to view the price of Bitcoin in (USD or EURO) so that I can view current and historical data in the main chart.
+
+As a user, I need each currency button to change the currency type displayed in the chart.
+
+As a user, I want to have a newsfeed that shows the current top stories related to Bitcoin and be able to navigate to them so that I can view them on another page. 
+
+As a user, I need a button that fetches and refreshes the data in the article newsfeed.
+
+![Dashboard Page Design](https://github.com/TheCodePack/bitcoin-price-monitor-api-capstone/blob/master/wireframes/dashboard-page.jpg)
+
+
+
---
+++ script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const apiKey = "6738fe19faf24362b2143d0ef9ec4f59";
+
+const searchURL = 'https://newsapi.org/v2/everything';
+
+// create string array from params object and join to create the query string in the url
+function formatArticleParam (params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+};
+
+function displayArticles (responseJson, resultsLimit) {
+    
+    console.log(responseJson);
+    $('#news-cont').empty();
+
+    for (let i=0; i < responseJson.articles.length & i < resultsLimit; i++) {
+        $('#news-cont').append(
+            `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
+            <p>${responseJson.articles[i].source.name}</p>
+            <p>By ${responseJson.articles[i].author}</p>
+            <p>${responseJson.articles[i].description}</p>
+            <img src='${responseJson.articles[i].urlToImage}'>
+            </li>` 
+        )};
+        
+        $('#news-cont').removeClass('hidden');
+}
+
+// fetch json data from the news API using API key and parameters 
+function getArticles(query, resultsLimit=5) {
+    const params = {
+        q: query,
+        language: "en"
+    };
+    const queryString = formatArticleParam(params)
+    const url = searchURL + '?' + queryString;
+
+    const options = {
+        headers: new Headers({
+            "X-API-Key": apiKey})
+    };
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayArticles
+        (responseJson,resultsLimit))
+        .catch(err => {
+            $('js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+};
+
+// Watch for newsfeed refresh
+function watchNewsForm () {
+    $('#news-button').click(event => {
+        event.preventDefault();
+        const newsFeedTerm = 'Bitcoin';
+        const resultsLimit = 5;
+        getArticles(newsFeedTerm, resultsLimit);
+    });
+};
+
+$(watchNewsForm);
